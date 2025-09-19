5cc04ae116932330b8d7a98c0a54f9b24a61b0eb
bajrangw <bajrangwavare@gmail.com>
2025-09-19 09:28:35 +0530

    Add @vercel/speed-insights dependency and integrate SpeedInsights component

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ThemeProvider } from "./components/context/ThemeContext.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
       <App />
+      <SpeedInsights />
     </ThemeProvider>
   </StrictMode>
 );
